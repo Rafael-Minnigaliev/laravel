d3d179fe534cc6c68f824921e0c946e2f18f010f
Rafael-Minnigaliev <rvfael@bk.ru>
2022-08-13 02:14:16 +0500

    hw-1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}', function (string $name) {
+    return "Привет $name";
+});
+
+Route::get('/info', function () {
+    return "Новостной проект";
+});
+
+Route::get('/news/{newsId}', function (int $newsId) {
+    $newsList = [
+        'Новость-1',
+        'Новость-2',
+        'Новость-3',
+        'Новость-4',
+        'Новость-5',
+    ];
+    return $newsList[$newsId - 1];
+});
